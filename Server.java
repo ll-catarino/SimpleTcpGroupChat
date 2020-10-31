31e6769793e276879e2f3257f0867aeb8e451c61
Ubuntu <ubuntu@ip-172-26-6-32.eu-west-2.compute.internal>
2020-10-31 23:31:41 +0000

    mvp

---
+++ Server.java
@@ -0,0 +1,134 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+    private final int THREAD_LIMIT = 4;
+    private ServerSocket serverSocket;
+    private ArrayList<ClientHandler> clients;
+    ExecutorService executorService;
+
+    public Server(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        clients = new ArrayList<>();
+        executorService = Executors.newFixedThreadPool(THREAD_LIMIT);
+        listen();
+    }
+
+    private void listen() throws IOException {
+        while (true) {
+            ClientHandler c = new ClientHandler(serverSocket.accept());
+            clients.add(c);
+            executorService.execute(c);
+            System.out.println(clients.size());
+        }
+    }
+
+    private void sendAllExcept(ClientHandler sender, String message) {
+        if (message.equals(""))
+            return;
+        for (ClientHandler c : clients) {
+            if (!c.equals(sender)) {
+                c.sendMessage(sender.getName() + ": " + message);
+            }
+        }
+    }
+    private void sendPM(ClientHandler dest, String message) {
+        dest.sendMessage(message);
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        Server s = new Server(25565);
+    }
+
+
+    private class ClientHandler implements Runnable {
+        private Socket clientSocket;
+        private PrintWriter out;
+        private Scanner in;
+        private String name;
+
+        public ClientHandler(Socket clientSocket) throws SocketException {
+            this.clientSocket = clientSocket;
+            name = "unnamed";
+
+            try {
+                out = new PrintWriter(clientSocket.getOutputStream(), true);
+            } catch (IOException e) {
+                System.out.println("Error");
+            }
+            try {
+                in = new Scanner(clientSocket.getInputStream());
+            } catch (IOException e) {
+                System.out.println("Error");
+            }
+        }
+
+        @Override
+        public void run() {
+            name = askName();
+	    sendMessage("To Quit type \":q\"");
+            while (true) {
+                receiveMessage();
+            }
+        }
+        
+        private String askName() {
+	    sendMessage("Please Enter Your Name");
+            String name = in.nextLine();
+            if (name.equals(""))
+                askName();
+            return name;
+        }
+
+        private void receiveMessage() {
+            if (in.hasNext()) {
+                String message = in.nextLine();
+                if (message.startsWith(":")) {
+                    processCommand(message.substring(1));
+                } else {
+                    sendAllExcept(this, message);
+                }
+            }
+        }
+
+        private void processCommand(String command) {
+            switch (command.split(" ")[0]) {
+                case "q":
+                    quit();
+                    break;
+                case "pm":
+                    int i = Integer.parseInt(command.split(" ")[1]);
+                    String message = command.split(" ")[2];
+                    ClientHandler dest = clients.get(i);
+                    sendPM(dest, message); //todo
+            }
+        }
+
+        private void quit() {
+            try {
+                clientSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            clients.remove(this);
+        }
+
+        private void sendMessage(String message) {
+            out.print(message + "\n");
+            out.flush();
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+}
+
